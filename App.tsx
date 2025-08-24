2560d988bc2167277fde11e10890a16af78268b9
shuhs <shuhsuanchang.9@gmail.com>
2025-08-25 00:33:46 +0100

    app config

--- App.tsx
+++ App.tsx
@@ -4,30 +4,18 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Home from './Components/HomeScreen';
 import Life from './Components/Life';
 import { RootStackParamList } from './pages';
+import { StatusBar } from 'react-native';
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator 
-
-      initialRouteName="Home">
-        <Stack.Screen
-        options={{
-          headerShown: false
-        }} 
-        name="Home" 
-        component={Home}
-        />
-        <Stack.Screen 
-        options={{
-          headerShown: false
-        }} 
-        name="Life" 
-        component={Life} 
-        />
-      </Stack.Navigator>
+      <StatusBar translucent barStyle={'dark-content'} backgroundColor="transparent" />
+        <Stack.Navigator screenOptions={{headerShown: false}} initialRouteName="Home">
+          <Stack.Screen options={{headerShown: false}} name="Home" component={Home}/>
+          <Stack.Screen options={{ headerShown: false }} name="Life" component={Life} />
+        </Stack.Navigator>
     </NavigationContainer>
   );
 }
@@ -35,8 +23,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    paddingTop: 0,
+    backgroundColor: "#e7e7cdff"
   },
 });
